9921a0b02f20d1b13965e955020a4f4f30b22d01
nikhil <nikhil@urbanpiper.com>
2020-03-26 17:02:21 +0530

    Migrated to use viewsets and routers, dropped APIView
    This is to take advantage of the built in pagination support in
    django

--- core/views.py
+++ core/views.py
@@ -1,162 +1,40 @@
-from rest_framework.views import APIView
+import structlog
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_204_NO_CONTENT
+from rest_framework.status import HTTP_400_BAD_REQUEST
 
 from .models import Merchant, Store, Item, Order
 from .seralizers import MerchantSerializer, StoreSerializer, ItemsSerializer, OrderSerializer
 from .tasks import save_orders
-import structlog
 
 logger = structlog.getLogger(__name__)
 
 
-class MerchantView(APIView):
+class MerchantViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
-
-    def get(self, request, pk=None, *args, **kwargs):
-        if pk is None:
-            queryset = Merchant.objects.all()
-            serializer = MerchantSerializer(queryset, many=True)
-            logger.info("merchant_get", all_items=True, request_data=request.data)
-            return Response(serializer.data)
-        else:
-            try:
-                merchant = Merchant.objects.get(pk=pk)
-            except Merchant.DoesNotExist:
-                return Response(status=HTTP_400_BAD_REQUEST)
-            serializer = MerchantSerializer(merchant)
-            logger.info("merchant_get", all_items=False, merchant_id=merchant.id, request_data=request.data)
-            return Response(serializer.data)
-
-    def post(self, request, *args, **kwargs):
-        serializer = MerchantSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            logger.info("merchant_save", request_data=request.data)
-            return Response({"message": "merchant saved"}, status=HTTP_201_CREATED)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
-    def put(self, request, pk, *args, **kwargs):
-        mc = Merchant.objects.get(pk=pk)
-        items = Item.objects.filter(merchant=mc)
-        serializer = MerchantSerializer(mc, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            logger.info("merchant_edit", request_data=request.data)
-            return Response(serializer.data)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, *args, **kwargs):
-        merchant = Merchant.objects.get(pk=pk)
-        merchant.delete()
-        logger.info("merchant_delete", request_data=request.data)
-        return Response(status=HTTP_204_NO_CONTENT)
+    queryset = Merchant.objects.all()
+    serializer_class = MerchantSerializer
 
 
-class StoresView(APIView):
+class StoreViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
-
-    def get(self, request, pk=None, *args, **kwargs):
-        if pk is None:
-            queryset = Store.objects.all()
-            serializer = StoreSerializer(queryset, many=True)
-            logger.info("store_get", all_items=True, request_data=request.data)
-            return Response(serializer.data)
-        else:
-            try:
-                store = Store.objects.get(pk=pk)
-            except Store.DoesNotExist:
-                return Response(status=HTTP_400_BAD_REQUEST)
-            logger.info("store_detail", all_items=False, store_id=store.id, request_data=request.data)
-            serializer = StoreSerializer(store)
-            return Response(serializer.data)
-
-    def post(self, request, *args, **kwargs):
-        serializer = StoreSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            logger.info("store_save", request_data=request.data)
-            return Response({"message": "Store saved"}, status=HTTP_201_CREATED)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
-    def put(self, request, pk, *args, **kwargs):
-        store = Store.objects.get(pk=pk)
-        serializer = StoreSerializer(store, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            logger.info("store_edit", request_data=request.data)
-            return Response(serializer.data)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, *args, **kwargs):
-        store = Store.objects.get(pk=pk)
-        store.delete()
-        logger.info("store_delete", request_data=request.data)
-        return Response(status=HTTP_204_NO_CONTENT)
+    queryset = Store.objects.all()
+    serializer_class = StoreSerializer
 
 
-class ItemsView(APIView):
+class ItemViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
-
-    def get(self, request, pk=None, *args, **kwargs):
-        if pk is None:
-            queryset = Item.objects.all()
-            serializer = ItemsSerializer(queryset, many=True)
-            logger.info("item_get", all_items=True, request_data=request.data)
-            return Response(serializer.data)
-        else:
-            try:
-                item = Item.objects.get(pk=pk)
-            except Item.DoesNotExist:
-                return Response(status=HTTP_400_BAD_REQUEST)
-            logger.info("item_get", all_items=False, request_data=request.data)
-            serializer = ItemsSerializer(item)
-            return Response(serializer.data)
-
-    def post(self, request, *args, **kwargs):
-        serializer = ItemsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            logger.info("item_save", request_data=request.data)
-            return Response({"message": "Item saved"}, status=HTTP_201_CREATED)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
-    def put(self, request, pk, *args, **kwargs):
-        item = Item.objects.get(pk=pk)
-        serializer = ItemsSerializer(item, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            logger.info("item_edit", request_data=request.data)
-            return Response(serializer.data)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, *args, **kwargs):
-        item = Item.objects.get(pk=pk)
-        item.delete()
-        logger.info("item_delete", request_data=request.data)
-        return Response(status=HTTP_204_NO_CONTENT)
+    queryset = Item.objects.all()
+    serializer_class = ItemsSerializer
 
 
-class OrderView(APIView):
+class OrderViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
 
-    def get(self, request, pk=None, *args, **kwargs):
-        if pk is None:
-            queryset = Order.objects.all()
-            serializer = OrderSerializer(queryset, many=True)
-            logger.info("order_get", all_items=True)
-            return Response(serializer.data)
-        else:
-            try:
-                order = Order.objects.get(pk=pk)
-            except Order.DoesNotExist:
-                return Response(status=HTTP_400_BAD_REQUEST)
-            serializer = OrderSerializer(order)
-            logger.info("order_get", all_items=False, order_id=order.id)
-            return Response(serializer.data)
-
-    def post(self, request, *args, **kwargs):
+    def create(self, request, *args, **kwargs):
         serializer = OrderSerializer(data=request.data)
         if serializer.is_valid():
             # serializer.save()
