9344014295c99d5cf499d2d29442913930b512aa
nikhil <nikhil@urbanpiper.com>
2020-03-31 19:56:45 +0530

    Adding new allowed host

--- updjango/settings.py
+++ updjango/settings.py
@@ -6,7 +6,8 @@ DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
 ALLOWED_HOSTS = ["13.127.81.33",
-                 "127.0.0.1",]
+                 "127.0.0.1",
+                 "116.203.137.67",]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
