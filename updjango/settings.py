d555d2e72b206705a01ed9a59422c9cfba9fab61
nikhil <nikhil@urbanpiper.com>
2020-04-04 01:06:03 +0530

    Added pagination max

--- updjango/settings.py
+++ updjango/settings.py
@@ -103,10 +103,11 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'MAX_PAGINATE_BY': 100,
 }
 
-# Celery url
+# Celery broker URL
 CELERY_BROKER_URL = 'amqp://localhost'
 
 SIMPLE_JWT = {
