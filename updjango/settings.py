c85e1320d2b0069ad633054e731ae5fa9374fac9
nikhil <nikhil@urbanpiper.com>
2020-04-04 02:20:26 +0530

    Moved to offset limit pagination class

--- updjango/settings.py
+++ updjango/settings.py
@@ -102,9 +102,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'MAX_PAGINATE_BY': 100,
+    # 'MAX_PAGINATE_BY': 100,
 }
 
 # Celery broker URL
